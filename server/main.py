730a82ad98e8e3fb4f379e1ab6b5f969dc4da57f
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-02-08 04:50:39 +0800

    Code entrypoint for testing

--- server/main.py
+++ server/main.py
@@ -1,9 +1,28 @@
-from app import create_app
+# from app import create_app
 
-def main():
-	my_app = create_app()
-	my_app.run(debug=True, port=5000)
+# def main():
+# 	my_app = create_app()
+# 	my_app.run(debug=True, port=5000)
 
+# from analyzer.datatypes.js_file import JsFile
+# from analyzer.datatypes.page import Page
+# from analyzer.features.javascript.static.func_search_count import FuncSearchCount
+
+# from analyzer.controllers.features_controller import FeaturesController
+# import analyzer
+# 
+# import analyzer
+# from analyzer.features.javascript.static import *
+
+
+from analyzer.controllers.features_controller import FeaturesController
 
 if __name__ == '__main__':
-	main()
+	# t = JsFile()
+	# p = Page()
+	
+	C = FeaturesController()
+
+	print(C.get_static_features())
+
+
