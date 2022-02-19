5fc9884ae05b9b04208d06a79a10dba54ca582ce
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-02-19 19:28:42 +0800

    Create 39_file_entropy_value.py

---
+++ server/analyzer/features/javascript/static/39_file_entropy_value.py
@@ -0,0 +1,33 @@
+from analyzer.core.js_extraction_patterns import JsExtractionPatterns
+from analyzer.abstracts.feature import Feature
+from analyzer.config import JS_RESERVED_WORDS_PATH
+from analyzer.core.utils import entropy as entropy_value
+import re
+
+class FileEntropyValue(Feature):
+
+	_index_no: int = 21
+	_name: str = "file_entropy_value"
+	_var_type: type = int
+
+	def _evaluate(self, js_buffer):
+		return entropy_value(js_buffer)
+
+	def extract(self, js_buffer):
+		return self._evaluate(js_buffer)
+
+	@property
+	def index_no(self):
+		return self._index_no
+
+	@property
+	def name(self):
+		return self._name	
+
+	@property
+	def var_type(self):
+		return self._var_type
+
+	def load_reserved_words(self):
+		with open(JS_RESERVED_WORDS_PATH) as f:
+			return [line.rstrip() for line in f]
