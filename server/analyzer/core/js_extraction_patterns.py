f615c402da51f5e1e41bc6c73b523e2b113df31e
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-02-19 19:26:14 +0800

    Regex pattern template generator for static features

---
+++ server/analyzer/core/js_extraction_patterns.py
@@ -0,0 +1,31 @@
+
+
+class JsExtractionPatterns:
+
+	@staticmethod
+	def var_function(function_name, optional_params=False):
+		if optional_params:
+			return ".{1,}%s\(.*\)" %(function_name)
+		return ".{1,}%s\(.+\)" %(function_name)
+
+	@staticmethod
+	def event(dot_call_str, event_listener_str):
+		return ".+\.%s.+function|addEventListener\([\'\"]%s[\'\"]" %(dot_call_str, event_listener_str)
+
+	@staticmethod
+	def normal_function(name):
+		return "%s\(.*\)" %(name)
+
+	@staticmethod
+	def files(extention):
+		return ".+\.%s" %(extention)
+# print(JsExtractionTypes.var_function("nigga"))
+
+import re
+test_str = "fireEvent(\"your mother\")"
+
+print(JsExtractionPatterns.normal_function("fireEvent"))
+
+
+
+print(JsExtractionPatterns.var_function("yo"))
