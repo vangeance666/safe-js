3abb2ebdf9c4b0a3a122d2b81d833f5e119fda2c
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-02-21 01:52:11 +0800

    Update js_file.py

--- server/analyzer/datatypes/js_file.py
+++ server/analyzer/datatypes/js_file.py
@@ -1,14 +1,26 @@
-from dataclasses import dataclass
-
-from analyzer.features.static
+from typing import List
+from dataclasses import dataclass, field
+from analyzer.abstracts.feature import Feature
 
 @dataclass
-class JsFile:	
-	file_name: str
-	content: str
+class JsFile:
 
-	static_features: str
-	dynamic_features: str
+	src: str = ""
+	content: str = ""
+
+	static_features: List[Feature] = field(default_factory=list)
+	dynamic_features: List[Feature] = field(default_factory=list)
+
+	# @classmethod	
+	# def parse_element(cls, bs_element):
+	# 	print("bs_element: ", bs_element)
+		
+	# 	if bs_element.has_attr('src'):			
+	# 		return cls(bs_element['src'])
+	# 	return cls()
+
+
+
 
 
 
