2ccdab44cf61467680abf391d3bfeaaea0bf3c01
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-02-23 05:11:42 +0800

    Modified event related features to analyze using new syntactic analysis

--- server/analyzer/features/javascript/static/10_func_charcodeat_count.py
+++ server/analyzer/features/javascript/static/10_func_charcodeat_count.py
@@ -1,7 +1,10 @@
 from analyzer.core.js_extraction_patterns import JsExtractionPatterns
 from analyzer.abstracts.feature import Feature
-import re
+from analyzer.datatypes.js_file import JsFile
 
+from analyzer.core.syntactic_helper import ConditionsFactory, parse_esprima
+
+import re
 
 class FuncCharcodeatCount(Feature):
 
@@ -9,13 +12,16 @@ class FuncCharcodeatCount(Feature):
 	_name: str = "func_charcodeat_count"
 	_var_type: type = int
 
-	PATTERN = JsExtractionPatterns.var_function("charCodeAt", True)
+	_description: str = "Returns the number of count for str.charCodeAt(idx)"
 
-	def _evaluate(self, js_buffer):
-		return len(re.findall(self.PATTERN, js_buffer))
+	CONDITIONS = [ConditionsFactory.element_func_call_condition("charCodeAt")]
+	# PATTERN = JsExtractionPatterns.var_function("charCodeAt", True)
 
-	def extract(self, js_buffer):
-		return self._evaluate(js_buffer)
+	def _evaluate(self, js_file: JsFile) -> int:
+		return sum(parse_esprima(js_file.body, cond) for cond in self.CONDITIONS)
+
+	def extract(self, js_file: JsFile):
+		return self._evaluate(js_file)
 
 	@property
 	def index_no(self):
--- server/analyzer/features/javascript/static/12_event_onload_count.py
+++ server/analyzer/features/javascript/static/12_event_onload_count.py
@@ -1,6 +1,8 @@
+import re
+from analyzer.datatypes.js_file import JsFile
 from analyzer.core.js_extraction_patterns import JsExtractionPatterns
 from analyzer.abstracts.feature import Feature
-import re
+from analyzer.core.syntactic_helper import ConditionsFactory, parse_esprima
 
 
 class EventOnloadCount(Feature):
@@ -9,13 +11,18 @@ class EventOnloadCount(Feature):
 	_name: str = "event_onload_count"
 	_var_type: type = int
 
-	PATTERN = JsExtractionPatterns.event("onload", "load")
+	CONDITIONS = [
+		ConditionsFactory.add_event_listener_condition("load")
+		, ConditionsFactory.on_event_assign_condition("onload")
+	]
 
-	def _evaluate(self, js_buffer):
-		return len(re.findall(self.PATTERN, js_buffer))
+	# PATTERN = JsExtractionPatterns.event("onload", "load")
 
-	def extract(self, js_buffer):
-		return self._evaluate(js_buffer)
+	def _evaluate(self, js_file: JsFile) -> int:
+		return sum(parse_esprima(js_file.body, cond) for cond in self.CONDITIONS)
+
+	def extract(self, js_file: JsFile):
+		return self._evaluate(js_file)
 
 	@property
 	def index_no(self):
