d40d523f1ff4327468b301c940ccdcf8f0c39acf
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-03-06 02:52:01 +0800

    Added extra page variables to keep track of processing statuses

--- server/analyzer/datatypes/page.py
+++ server/analyzer/datatypes/page.py
@@ -1,11 +1,19 @@
 from typing import List, Optional
 from dataclasses import dataclass, field
 from analyzer.datatypes.js_file import JsFile
+from bs4 import ResultSet
 
 @dataclass(order=True)
 class Page:
-	url: str = field(default="")
+	src: str = field(default="")
+	text: str = field(default="")
+
+	script_elements: list = field(default_factory=list)
+
 	internal_js_files: List[JsFile] = field(default_factory=list)
 	external_js_files: List[JsFile] = field(default_factory=list)
 
-	success: bool = field(default=False)
+	success: bool 	= field(default=False)
+	parsed: bool 	= field(default=False)
+	extracted: bool = field(default=False)
+	saved: bool 	= field(default=False)
