309e4e6be340f13c111c876de0260733700be8ea
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-03-20 05:43:07 +0800

    Rename to static_features to prevent potential collision

--- server/analyzer/datatypes/page.py
+++ server/analyzer/datatypes/page.py
@@ -1,13 +1,13 @@
+from analyzer.datatypes.js_file import JsFile
+
 from typing import List, Optional
 from dataclasses import dataclass, field
-from analyzer.datatypes.js_file import JsFile
 from bs4 import ResultSet
 
 @dataclass(order=True)
 class Page:
 	src: str = field(default="")
 	text: str = field(default="")
-
 	script_elements: list = field(default_factory=list)
 
 	internal_js_files: List[JsFile] = field(default_factory=list)
