2a7f005e224791879364a4a89c6f672df9d98152
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-03-20 05:39:06 +0800

    Moved config.py to root folder and refactor variables

---
+++ server/config.py
@@ -0,0 +1,11 @@
+import os
+
+JS_RESERVED_WORDS_PATH = os.path.join(os.getcwd(), "analyzer\\resources\\reserved_words.txt")
+# "C:\Users\User\Documents\GitHub\safe-js\server\analyzer\resources\reserved_words.txt"
+
+PAGE_SAVE_FLDR = os.path.join(os.getcwd(), "data\\pages\\")
+
+DYNAMIC_DUMP_FLDR = os.path.join(os.getcwd(), "data\\js_dynamic_results\\")
+
+
+RESULTS_SAVED_PATH = os.path.join(os.getcwd(), "save\\database.pickle")
