a66c02a30041b6b81285848d3e2fac0124932b55
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-03-23 04:49:13 +0800

    Update config.py

--- server/config.py
+++ server/config.py
@@ -1,6 +1,6 @@
 import os
 
-JS_RESERVED_WORDS_PATH = os.path.join(os.getcwd(), "analyzer\\resources\\reserved_words.txt")
+JS_RESERVED_WORDS_PATH = os.path.join(os.getcwd(), "analyzer" , "resources", "reserved_words.txt")
 # "C:\Users\User\Documents\GitHub\safe-js\server\analyzer\resources\reserved_words.txt"
 
 PAGE_SAVE_FLDR = os.path.join(os.getcwd(), "data", "pages")
