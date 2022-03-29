d2eaaa0148bde3e4bc4df71191ba8df73d5fd72c
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-03-29 18:44:21 +0800

    Each feature output will be in a tuple to represent if the value exists or error encountered

--- server/analyzer/core/js_features_extractor.py
+++ server/analyzer/core/js_features_extractor.py
@@ -1,10 +1,11 @@
 import os
 
+from analyzer.core.exceptions import InvalidUsageError
 from analyzer.datatypes.js_file import JsFile
-from analyzer.features.javascript.dynamic import ioc_features, url_features, active_url_features
-print("active_url_features: ", active_url_features)
-print("url_features: ", url_features)
-print("ioc_features: ", ioc_features)
+from analyzer.features.javascript.dynamic import (active_url_features, ioc_features, url_features)
+# print("active_url_features: ", active_url_features)
+# print("url_features: ", url_features)
+# print("ioc_features: ", ioc_features)
 from analyzer.features.javascript.static import static_features
 
 
@@ -20,35 +21,39 @@ class JsFeaturesExtractor:
 
 		for key in features:
 			feature_obj = features[key]()
-			ret[feature_obj.name] = feature_obj.extract(js_file)
+
+			try:
+				ret[feature_obj.name] = (1, feature_obj.extract(js_file))
+			except Exception as e:
+				print(e)
+				ret[feature_obj.name]  = (0,0) # 0 To represent error				
 
 		return ret
 
 	def extract_dynamic_features(self, js_file: JsFile):
 
 		if not js_file.dynamic_done:
-			raise Exception("Box-JS Analysis for this jsfile not done yet")
+			raise InvalidUsageError("Not analyzed with Box-jS yet, cant extract features")
 
-		js_file.dynamic_features["iocs"] = self._parse_features(ioc_features, js_file)
-		js_file.dynamic_features["urls"] = self._parse_features(url_features, js_file)
-		js_file.dynamic_features["active_urls"] = self._parse_features(active_url_features, js_file)
+		js_file.dynamic_features["iocs"] = self._parse_features(ioc_features
+			, js_file)
+
+		js_file.dynamic_features["urls"] = self._parse_features(url_features
+			, js_file)
+
+		js_file.dynamic_features["active_urls"] = self._parse_features(active_url_features
+			, js_file)		
 
 		js_file.dynamic_features_done = True
 
 	def extract_static_features(self, js_file: JsFile):
 
 		if not js_file.synthetic_done:
-			raise Exception("Synthetic details not extracted yet")
+			raise InvalidUsageError("Synthetic details not extracted yet.")
 
 		if not js_file.text:
 			raise ValueError("Invalid JS text attribute.")		
 
 		js_file.static_features["all"] = self._parse_features(static_features, js_file)
-
-		# print("Extracting static features for :", js_file.src)
-		# for x in self._static_features:
-		# 	print("Currently extracting: ", x)
-		# 	x_obj = self._static_features[x]()
-		# 	js_file.static_features["all"] = x_obj.extract(js_file)
 
 		js_file.static_features_done = True
