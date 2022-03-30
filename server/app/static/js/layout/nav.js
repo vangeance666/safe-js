94048ebbd2b27624febebe0b95713aef2c6b85cb
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-03-30 21:39:21 +0800

    Update nav.js

--- server/app/static/js/layout/nav.js
+++ server/app/static/js/layout/nav.js
@@ -3,8 +3,31 @@ layout.nav = (function() {
 	let self = {};
 
 
-	var sideBarItem = function() {
-
+	var sideBarItem = function(iconCls, labelName) {
+		return ["li", {"class": "nav-item "},
+            ["a", {"href": "/transactions.html", "class": "nav-link"},
+                ["span", {"class": "sidebar-icon"},
+                    ["svg", {
+                            "class": "icon icon-xs me-2",
+                            "fill": "currentColor",
+                            "viewbox": "0 0 20 20",
+                            "xmlns": "http://www.w3.org/2000/svg"
+                        },
+                        ["path", {
+                                "d": "M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z"
+                            }
+                        ],
+                        ["path", {
+                                "fill-rule": "evenodd",
+                                "d": "M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z",
+                                "clip-rule": "evenodd"
+                            }
+                        ]
+                    ]
+                ],
+                ["span", {"class": "sidebar-text"}, labelName]
+            ]
+        ];
 	}
 
 	let mainNavCtx = ["nav", {"class": "navbar navbar-dark navbar-theme-primary px-4 col-12 d-lg-none"},
@@ -38,6 +61,7 @@ layout.nav = (function() {
 	        ]
 	    ]
 	];
+
 
 
 
@@ -172,295 +196,9 @@ layout.nav = (function() {
                     ],
                     ["span", {"class": "sidebar-text"}, "Transactions"]
                 ]
-            ],
-            ["li", {"class": "nav-item "},
-                ["a", {"href": "/settings.html", "class": "nav-link"},
-                    ["span", {"class": "sidebar-icon"},
-                        ["svg", {
-                                "class": "icon icon-xs me-2",
-                                "fill": "currentColor",
-                                "viewbox": "0 0 20 20",
-                                "xmlns": "http://www.w3.org/2000/svg"
-                            },
-                            ["path", {
-                                    "fill-rule": "evenodd",
-                                    "d": "M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z",
-                                    "clip-rule": "evenodd"
-                                }
-                            ]
-                        ]
-                    ],
-                    ["span", {"class": "sidebar-text"}, "Settings"]
-                ]
-            ],
-            ["li", {"class": "nav-item "},
-                ["span", {
-                        "class": "nav-link  collapsed  d-flex justify-content-between align-items-center",
-                        "data-bs-toggle": "collapse",
-                        "data-bs-target": "#submenu-app"
-                    },
-                    ["span",
-                        ["span", {"class": "sidebar-icon"},
-                            ["svg", {
-                                    "class": "icon icon-xs me-2",
-                                    "fill": "currentColor",
-                                    "viewbox": "0 0 20 20",
-                                    "xmlns": "http://www.w3.org/2000/svg"
-                                },
-                                ["path", {
-                                        "fill-rule": "evenodd",
-                                        "d": "M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z",
-                                        "clip-rule": "evenodd"
-                                    }
-                                ]
-                            ]
-                        ],
-                        ["span", {"class": "sidebar-text"}, "Tables"]
-                    ],
-                    ["span", {"class": "link-arrow"},
-                        ["svg", {
-                                "class": "icon icon-sm",
-                                "fill": "currentColor",
-                                "viewbox": "0 0 20 20",
-                                "xmlns": "http://www.w3.org/2000/svg"
-                            },
-                            ["path", {
-                                    "fill-rule": "evenodd",
-                                    "d": "M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z",
-                                    "clip-rule": "evenodd"
-                                }
-                            ]
-                        ]
-                    ]
-                ],
-                ["div", {
-                        "class": "multi-level collapse ",
-                        "role": "list",
-                        "id": "submenu-app",
-                        "aria-expanded": "false"
-                    },
-                    ["ul", {"class": "flex-column nav"},
-                        ["li", {"class": "nav-item "},
-                            ["a", {
-                                    "class": "nav-link",
-                                    "href": "/tables-bootstrap-tables.html"
-                                },
-                                ["span", {"class": "sidebar-text"},
-                                    "Bootstrap Tables"
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ],
-            ["li", {"class": "nav-item "},
-                ["span", {
-                        "class": "nav-link  collapsed  d-flex justify-content-between align-items-center",
-                        "data-bs-toggle": "collapse",
-                        "data-bs-target": "#submenu-pages"
-                    },
-                    ["span",
-                        ["span", {"class": "sidebar-icon"},
-                            ["svg", {
-                                    "class": "icon icon-xs me-2",
-                                    "fill": "currentColor",
-                                    "viewbox": "0 0 20 20",
-                                    "xmlns": "http://www.w3.org/2000/svg"
-                                },
-                                ["path", {
-                                        "fill-rule": "evenodd",
-                                        "d": "M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z",
-                                        "clip-rule": "evenodd"
-                                    }
-                                ],
-                                ["path", {
-                                        "d": "M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"
-                                    }
-                                ]
-                            ]
-                        ],
-                        ["span", {"class": "sidebar-text"}, "Page examples"]
-                    ],
-                    ["span", {"class": "link-arrow"},
-                        ["svg", {
-                                "class": "icon icon-sm",
-                                "fill": "currentColor",
-                                "viewbox": "0 0 20 20",
-                                "xmlns": "http://www.w3.org/2000/svg"
-                            },
-                            ["path", {
-                                    "fill-rule": "evenodd",
-                                    "d": "M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z",
-                                    "clip-rule": "evenodd"
-                                }
-                            ]
-                        ]
-                    ]
-                ],
-                ["div", {
-                        "class": "multi-level collapse  ",
-                        "role": "list",
-                        "id": "submenu-pages",
-                        "aria-expanded": "false"
-                    },
-                    ["ul", {"class": "flex-column nav"},
-                        ["li", {"class": "nav-item"},
-                            ["a", {
-                                    "class": "nav-link",
-                                    "href": "/page-sign-in.html"
-                                },
-                                ["span", {"class": "sidebar-text"}, "Sign In"]
-                            ]
-                        ],
-                        ["li", {"class": "nav-item"},
-                            ["a", {
-                                    "class": "nav-link",
-                                    "href": "/page-sign-up.html"
-                                },
-                                ["span", {"class": "sidebar-text"}, "Sign Up"]
-                            ]
-                        ],
-                        ["li", {"class": "nav-item"},
-                            ["a", {
-                                    "class": "nav-link",
-                                    "href": "/page-forgot-password.html"
-                                },
-                                ["span", {"class": "sidebar-text"},
-                                    "Forgot password"
-                                ]
-                            ]
-                        ],
-                        ["li", {"class": "nav-item"},
-                            ["a", {
-                                    "class": "nav-link",
-                                    "href": "/page-reset-password.html"
-                                },
-                                ["span", {"class": "sidebar-text"},
-                                    "Reset password"
-                                ]
-                            ]
-                        ],
-                        ["li", {"class": "nav-item"},
-                            ["a", {
-                                    "class": "nav-link",
-                                    "href": "/page-lock.html"
-                                },
-                                ["span", {"class": "sidebar-text"}, "Lock"]
-                            ]
-                        ],
-                        ["li", {"class": "nav-item"},
-                            ["a", {"class": "nav-link", "href": "/page-404.html"},
-                                ["span", {"class": "sidebar-text"},
-                                    "404 Not Found"
-                                ]
-                            ]
-                        ],
-                        ["li", {"class": "nav-item"},
-                            ["a", {"class": "nav-link", "href": "/page-500.html"},
-                                ["span", {"class": "sidebar-text"},
-                                    "500 Not Found"
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ],
-            ["li", {"class": "nav-item "},
-                ["span", {
-                        "class": "nav-link  collapsed  d-flex justify-content-between align-items-center",
-                        "data-bs-toggle": "collapse",
-                        "data-bs-target": "#submenu-components"
-                    },
-                    ["span",
-                        ["span", {"class": "sidebar-icon"},
-                            ["svg", {
-                                    "class": "icon icon-xs me-2",
-                                    "fill": "currentColor",
-                                    "viewbox": "0 0 20 20",
-                                    "xmlns": "http://www.w3.org/2000/svg"
-                                },
-                                ["path", {"d": "M4 3a2 2 0 100 4h12a2 2 0 100-4H4z"}
-                                ],
-                                ["path", {
-                                        "fill-rule": "evenodd",
-                                        "d": "M3 8h14v7a2 2 0 01-2 2H5a2 2 0 01-2-2V8zm5 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z",
-                                        "clip-rule": "evenodd"
-                                    }
-                                ]
-                            ]
-                        ],
-                        ["span", {"class": "sidebar-text"}, "Components"]
-                    ],
-                    ["span", {"class": "link-arrow"},
-                        ["svg", {
-                                "class": "icon icon-sm",
-                                "fill": "currentColor",
-                                "viewbox": "0 0 20 20",
-                                "xmlns": "http://www.w3.org/2000/svg"
-                            },
-                            ["path", {
-                                    "fill-rule": "evenodd",
-                                    "d": "M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z",
-                                    "clip-rule": "evenodd"
-                                }
-                            ]
-                        ]
-                    ]
-                ],
-                ["div", {
-                        "class": "multi-level collapse ",
-                        "role": "list",
-                        "id": "submenu-components",
-                        "aria-expanded": "false"
-                    },
-                    ["ul", {"class": "flex-column nav"},
-                        ["li", {"class": "nav-item "},
-                            ["a", {
-                                    "class": "nav-link",
-                                    "href": "/components-buttons.html"
-                                },
-                                ["span", {"class": "sidebar-text"}, "Buttons"]
-                            ]
-                        ],
-                        ["li", {"class": "nav-item "},
-                            ["a", {
-                                    "class": "nav-link",
-                                    "href": "/components-notifications.html"
-                                },
-                                ["span", {"class": "sidebar-text"},
-                                    "Notifications"
-                                ]
-                            ]
-                        ],
-                        ["li", {"class": "nav-item "},
-                            ["a", {
-                                    "class": "nav-link",
-                                    "href": "/components-forms.html"
-                                },
-                                ["span", {"class": "sidebar-text"}, "Forms"]
-                            ]
-                        ],
-                        ["li", {"class": "nav-item "},
-                            ["a", {
-                                    "class": "nav-link",
-                                    "href": "/components-modals.html"
-                                },
-                                ["span", {"class": "sidebar-text"}, "Modals"]
-                            ]
-                        ],
-                        ["li", {"class": "nav-item "},
-                            ["a", {
-                                    "class": "nav-link",
-                                    "href": "/components-typography.html"
-                                },
-                                ["span", {"class": "sidebar-text"},
-                                    "Typography"
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ],
+            ],            
+          
+            
             ["li", {
                     "role": "separator",
                     "class": "dropdown-divider mt-4 mb-3 border-gray-700"
@@ -543,15 +281,16 @@ layout.nav = (function() {
         ]
     ]
 ]
+
 	
 	self.ctx = [mainNavCtx, sideNavCtx]
 
 	self.addEvents = function() {
-		$('.'+eleClass['navBtn']).click(function(){
-			$('.'+eleClass['navBtn']).removeClass("active");
-			$(this).addClass("active");
-			console.log("Clicked on nav item");
-		});
+		// $('.'+eleClass['navBtn']).click(function(){
+		// 	$('.'+eleClass['navBtn']).removeClass("active");
+		// 	$(this).addClass("active");
+		// 	console.log("Clicked on nav item");
+		// });
 		console.log("nav added events");
 	}
 
