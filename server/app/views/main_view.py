501087355e0961f165098138fb99b91c04b62f8b
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-03-30 17:51:47 +0800

    Delete old flask views

--- server/app/views/main_view.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, request, url_for, redirect, session, jsonify, flash
-
-main_view = Blueprint("main_view", __name__)
-
-@main_view.route('/')
-def root():
-	return jsonify("home"), 200
