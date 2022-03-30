48add1d28b31fcde4c23e7cbf7ffc4c38476ccf4
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-03-30 21:39:23 +0800

    Update init.js

--- server/app/static/js/init.js
+++ server/app/static/js/init.js
@@ -35,94 +35,91 @@ var modelStats = {
 }
 
 const ids = {
+	"root": "id-root",
+	"rootBody": "id-root-body"
 
 };
 
 
-const routeMapping = {
-	"dashboard": layout.pages.dashboard,
-	"recent": layout.pages.recent,
-	"analysis": layout.pages.analysis
-}
-
-
-let loadPage = function() {
-	if (window.location.hash) {
-		let hash = window.location.hash.substring(1);
-		if (routeMapping[hash] !== undefined) {
-			routeMapping.display()
-		}
-		
-	} 
-}
-
-
-// window.needsUpdate = {
-// 	submitPromptFile:false, submitSendFile:false, getMachineNames:false,
-// 	homeRetrieveSubmissions:false,
-
-// 	taskLoadTaskData:false, taskDisableResubmission: false, taskEnableResubmission: false,
-// 	taskForceResubmit:false,
-
-// 	machinesRetrieveData:false
-// }
-
-
-// toastr.options = {
+toastr.options = {
 	  
-// 	  "debug": false,
-// 	  "newestOnTop": false,
-// 	  "progressBar": false,
-// 	  "positionClass": "toast-top-right",
-// 	  "preventDuplicates": false,
-// 	  "onclick": null,
-// 	  "showDuration": "200",
-// 	  "hideDuration": "1000",
-// 	  "timeOut": "3000",
-// 	  "extendedTimeOut": "1000",
-// 	  "showEasing": "swing",
-// 	  "hideEasing": "linear",
-// 	  "showMethod": "fadeIn",
-// 	  "hideMethod": "fadeOut",
-// 	  "preventDuplicates": true
-// };
+	  "debug": false,
+	  "newestOnTop": false,
+	  "progressBar": false,
+	  "positionClass": "toast-top-right",
+	  "preventDuplicates": false,
+	  "onclick": null,
+	  "showDuration": "200",
+	  "hideDuration": "1000",
+	  "timeOut": "3000",
+	  "extendedTimeOut": "1000",
+	  "showEasing": "swing",
+	  "hideEasing": "linear",
+	  "showMethod": "fadeIn",
+	  "hideMethod": "fadeOut",
+	  "preventDuplicates": true
+};
 
 
-// showSuccess = function(s, title) {
-// 	if (title) {
-// 		toastr.success(s, title);
-// 	}
-// 	else {
-// 		toastr.success(s);
-// 	}
-// }
+showSuccess = function(s, title) {
+	if (title) {
+		toastr.success(s, title);
+	}
+	else {
+		toastr.success(s);
+	}
+}
 
 
-// showInfo = function(t){ 
-// 	toastr.info(t);
-// }
+showInfo = function(t){ 
+	toastr.info(t);
+}
 
-// showLoader = function(t) {
-//   // $('#refresh').addClass('disabled');
-//   $.LoadingOverlay('show', {
-//     image: '',
-//     custom: $(HTML(['div', {id: 'loader-animation', class: 'row noselect justify-content-center d-flex'},
-//       ['img', {class: ''}, {src: 'resources/loader.gif'}],
-//       ['span', {class: ' h4 col-12 ml-2 text-center '}, t]
-//     ]))
-//   });
-// }
+showLoader = function(t) {
+  // $('#refresh').addClass('disabled');
+  $.LoadingOverlay('show', {
+    image: '',
+    custom: $(HTML(['div', {id: 'loader-animation', class: 'row noselect justify-content-center d-flex'},
+      ['img', {class: ''}, {src: 'resources/loader.gif'}],
+      ['span', {class: ' h4 col-12 ml-2 text-center '}, t]
+    ]))
+  });
+}
 
-// showError = function(e) {
-// 	console.log("e is:"+e);
-// 	if (e != '') {
-// 		toastr.error(e);
-// 	}
-// }
+showError = function(e) {
+	console.log("e is:"+e);
+	if (e != '') {
+		toastr.error(e);
+	}
+}
 
-// hideLoader = function(error) {
-//   if (error) toastr.error(error);
-//   $('#refresh').removeClass('disabled');
-//   $.LoadingOverlay('hide');
-// }
+hideLoader = function(error) {
+  if (error) toastr.error(error);
+  $('#refresh').removeClass('disabled');
+  $.LoadingOverlay('hide');
+}
 
+window.pageRouteMapping = function(){
+	return	{
+		"dashboard": layout.pages.dashboard,
+		"recent": layout.pages.recent,
+		"analysis": layout.pages.analysis		
+	}
+} 	
+
+
+renderRoute = function() {
+	if (!window.location.hash) {
+		window.pageRouteMapping.dashboard.display()	
+		return
+	}
+
+	let hash = window.location.hash.substring(1);
+	console.log("hash: ", hash);
+	if (window.pageRouteMapping[hash] !== undefined) {
+		window.pageRouteMapping.display()
+	} else {
+		console.log("printing default dashboard")
+		window.pageRouteMapping.dashboard.display();
+	}
+}
