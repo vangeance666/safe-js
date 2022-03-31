f78a13b918e865d7b5400a8e24cb5209ad77fb26
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-04-01 01:01:21 +0800

    Update main.js

--- server/app/static/js/main.js
+++ server/app/static/js/main.js
@@ -3,19 +3,27 @@ $(function(){
 
 	$('body').html(HTML(
 		layout.nav.ctx,
-		['div', {'id': eleIds['root'], 'class':'container-fluid'}, 
-			['div', {'id': eleIds['rootBody'], 'class': 'container-fluid d-flex flex-column min-vh-100'}], // pages only change this
-		],
-		layout.footer.ctx
+		["div", {"class": "container-fluid", "id": eleIds['root']},
+		    ["main", {"id": eleIds['rootMain'], "class": "content"},
+		    	layout.banner.ctx,
+		    	["div", {"class": "d-flex flex-column min-vh-100",
+		            "id": eleIds['rootBody']}
+		        ],
+		        layout.footer.ctx
+		        
+		    ]
+		    
+		]
+		
 	));
-	
+
 	layout.nav.addEvents();
-	
-	HTML.route("", layout.pages.dashboard.display);
+		
+	// HTML.route("", layout.pages.dashboard.display);
 	HTML.route("dashboard", layout.pages.dashboard.display);
 	HTML.route("recent", layout.pages.recent.display);
 	HTML.route("analysis", layout.pages.analysis.display);
 	HTML.route("settings", layout.pages.settings.display);
 
-	HTML.route.go();
+	HTML.route.go(":dashboard");
 });
