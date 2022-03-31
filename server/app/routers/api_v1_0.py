d41df8d0163faf3f8efcf65555e03e5fa4512933
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-03-31 20:12:07 +0800

    Placeholder data values to return when requesting for recent results

--- server/app/routers/api_v1_0.py
+++ server/app/routers/api_v1_0.py
@@ -5,21 +5,28 @@ from app.models.analysis_request import AnalysisRequest
 
 router = APIRouter()
 
+
 @router.get("/analysis/statistics/")
 async def get_dashboard_stats():
-	return JSONResponse(content={"no_of_tasks": 2, "analyzed_count": 5})
+    return JSONResponse(content={"no_of_tasks": 2, "analyzed_count": 5})
+
 
 @router.get("/analysis/results/")
 async def get_past_results():
-	return JSONResponse(content=json_compatible_item_data)
+    return JSONResponse(content={
+        "status": "ok", "headers": ["Page URL", "JS Src", "Static analysis Status", "Dynamic analysis Status", "Prediction"], "rows": [
+            ["www.facebook.com", 'jomama source', 'Done', 'Done', '50']
+        ]}
+    )
+
 
 @router.post("/analysis/run/")
-async def analyze_url(analysis_request: AnalysisRequest=Body(...)):
-	# Add analysis to queue and return analysis id 
-    return JSONResponse(content={"status" :"ok","task_id": 1})
+async def analyze_url(analysis_request: AnalysisRequest = Body(...)):
+    # Add analysis to queue and return analysis id
+    return JSONResponse(content={"status": "ok", "task_id": 1})
+
 
 @router.get("/analysis/run/status/")
 async def analysis_status(id: int = 0):
-	# Pending|Running|Done
-    return JSONResponse(content={"status": "found", "task_status": "Pending", "task_id": 1})    
-
+    # Pending|Running|Done
+    return JSONResponse(content={"status": "found", "task_status": "Pending", "task_id": 1})
