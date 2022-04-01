a17d2c6dedfc9359e6e34660ce9e91c25804c45c
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-04-02 06:34:59 +0800

    Update platform_controller.py

--- server/app/platform_controller.py
+++ server/app/platform_controller.py
@@ -1,4 +1,5 @@
 import itertools
+from collections import deque
 from dataclasses import asdict
 from typing import List
 
@@ -9,7 +10,7 @@ from analyzer.controllers.results_controller import ResultsController
 from analyzer.core.dataset_generator import DatasetGenerator
 from analyzer.datatypes.js_file import JsFile
 from analyzer.datatypes.page import Page
-from collections import deque
+
 
 class NotFoundDetails(Exception):
 	pass
@@ -40,10 +41,10 @@ class PlatformController:
 		return {
 			"pages_analysed": len(self._analyzed_pages)
 			, "js_file_analysed": len(all_js_files)
-			, "predict_flagged_files": 0
+			, "predict_flagged_files": 0 # Remember to implement logic
 		}
 
-	# ok
+	# ok for recent view
 	def fetch_all_pages_details(self) -> list:
 
 		ret = []
@@ -52,31 +53,31 @@ class PlatformController:
 			row = {}
 			row['id'] = page.id
 			row['page_url'] = page.src
-			js_files = itertools.chain(page.internal_js_files, page.external_js_files)
+			js_files = list(itertools.chain(page.internal_js_files, page.external_js_files))
 			row['static_done'] = all([js_file.static_done for js_file in js_files])
 			row['dynamic_done'] = all([js_file.dynamic_done for js_file in js_files])
-			row['js_file_srcs'] = [js_file.src for js_file in js_files]
+
+			row['js_file_details'] = [ {
+				"id":js_file.id
+				, "src":js_file.src
+				, "static_features_done" : js_file.static_features_done
+				, "dynamic_features_done" : js_file.dynamic_features_done
+				} for js_file in js_files]
+				
 			ret.append(row)
 		return ret
-				
-	def fetch_js_file_details(self, page_id: int, js_src: str) -> dict:
-		ret = {
-			"page_id": page_id
-			, "js_src": js_src
-			, "details": ""
-		}
+
+
+	# For analysis view
+	def fetch_js_file_details(self, page_id: int, js_file_id: int) -> dict:
+		# ret = {
+		# 	"page_id": page_id
+		# 	, "js_file_id": js_file_id
+		# }
 		for page in self._analyzed_pages:
 			if page.id == page_id:
 				for js_file in itertools.chain(page.internal_js_files, page.external_js_files):
-					if js_file.src == js_src:
-						ret['details'] = asdict(js_file)
+					if js_file.id == js_file_id:
+						return asdict(js_file)
 
-		if not ret['details']:
-			raise NotFoundDetails("No js file details found with page id {} and js_src {}".format(page_id, js_src))
-
-		return ret
-
-
-
-
-
+		return None
