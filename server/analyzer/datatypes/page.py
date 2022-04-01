82ec90e706ea4721d3a55875db4e94b172f9f662
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-04-01 20:37:48 +0800

    Add id to page

--- server/analyzer/datatypes/page.py
+++ server/analyzer/datatypes/page.py
@@ -1,20 +1,24 @@
-from analyzer.datatypes.js_file import JsFile
-
-from typing import List, Optional
 from dataclasses import dataclass, field
+from itertools import count
+from typing import List, Optional
+
+from analyzer.datatypes.js_file import JsFile
 from bs4 import ResultSet
+
 
 @dataclass
 class Page:
+	id: int = field(default_factory=count().__next__, init=False)
 	src: str = field(default="")
 	text: str = field(default="")
-	
+
 	script_elements: list = field(default_factory=list)
 
 	internal_js_files: List[JsFile] = field(default_factory=list)
 	external_js_files: List[JsFile] = field(default_factory=list)
 
-	success: bool 	= field(default=False)
-	parsed: bool 	= field(default=False)
-	extracted: bool = field(default=False)
-	saved: bool 	= field(default=False)
+	success: bool 		= field(default=False)
+	parsed: bool 		= field(default=False)
+	extracted: bool 	= field(default=False)
+	saved: bool 		= field(default=False)
+	is_analyzed: bool	= field(default=False)
