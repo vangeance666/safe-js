1465fc22bef824bb6285b951370dc6d790a2312f
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-04-03 01:20:36 +0800

    Update config.py

--- server/config.py
+++ server/config.py
@@ -6,4 +6,5 @@ PAGE_SAVE_FLDR = os.path.join(os.getcwd(), "data", "pages")
 
 DYNAMIC_DUMP_FLDR = os.path.join(os.getcwd(), "data", "js_dynamic_results")
 
-RESULTS_SAVED_PATH = os.path.join(os.getcwd(), "save",  "database.pickle")
+ANALYZED_PAGES_SAVE_PATH = os.path.join(os.getcwd(), "save",  "database.pickle")
+PENDING_PAGES_SAVE_PATH = os.path.join(os.getcwd(), "save", "pending.pickle")
