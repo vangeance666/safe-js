20d274efcfc2bc6ea49aa534e0f618064e55caad
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-04-03 23:34:56 +0800

    Analysis controller allows overriding of dynamic analysis dump folder for manual use

--- server/analyzer/controllers/analysis_controller.py
+++ server/analyzer/controllers/analysis_controller.py
@@ -7,37 +7,53 @@ from analyzer.datatypes.js_file import JsFile
 
 from analyzer.core.js_static_analyzer import JsStaticAnalyzer
 
+from config import DYNAMIC_DUMP_FLDR
 
 
 class AnalysisController:
 
-	def __init__(self):
-		self._dynamic_analyzer = JsDynamicAnalyzer()
-		self._static_analyzer = JsStaticAnalyzer()
+	def __init__(self, dynamic_dump_folder=None):
 
-		self.analyzers = [
-			self._static_analyzer
-			, self._dynamic_analyzer
-		]
+		self._static_analyzer = JsStaticAnalyzer()
+		self._dynamic_analyzer = JsDynamicAnalyzer(dynamic_dump_folder or DYNAMIC_DUMP_FLDR)
 
 	def __enter__(self):
 		return self
 
 	def __exit__(self, type, value, traceback):
-		self._dynamic_analyzer.cleanup()		
+		self._dynamic_analyzer.cleanup()
 
-	def _apply_all_analyzers(self, js_file: JsFile) -> list:
+	def analyze_js_file(self, js_file: JsFile) -> list[bool]:
 		return [analyzer.run(js_file) for analyzer in self.analyzers]	
 
-	# New
-	def analyze_page_js_files(self, page: Page) -> bool:
+	def run_static_analysis(self, js_file: JsFile):
+		try:
+			self._static_analyzer.run(js_file)
+		except Exception as e:
+			print("e: ", e)
+			js_file.static_run_error = True
 
-		if not page.success:
-			return False
+	def run_dynamic_analysis(self, js_file: JsFile):
+		try:
+			self._dynamic_analyzer.run(js_file)
+		except Exception as e:
+			print("e: ", e)
+			js_file.dynamic_run_error = True
+	# New
+	# def analyze_page_js_files(self, page: Page) -> bool:
+
+	# 	if not page.success:
+	# 		return False
+
+	# 	for js_file in itertools.chain(page.internal_js_files, page.external_js_files):
+	# 		self.run_static_analysis(js_file)
+	# 		self.run_dynamic_analysis(js_file)
+
+	# 	return True
 
 		# Analyze regardless of erorr
-		return all([self._apply_all_analyzers(js_file) 
-			for js_file in itertools.chain(page.internal_js_files, page.external_js_files)])
+		# return all([self.analyze_js_file(js_file) 
+		# 	for js_file in itertools.chain(page.internal_js_files, page.external_js_files)])
 
 	# Olds
 	# def analyze_pages_js_files(self, pages: List[Page]) -> bool:
