40d9c692436c6c466609b615cc949cbc8cd75208
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-04-03 15:51:18 +0800

    Static analyzer now retries to parse js file with json dumps

--- server/analyzer/core/js_static_analyzer.py
+++ server/analyzer/core/js_static_analyzer.py
@@ -1,12 +1,20 @@
-from analyzer.datatypes.js_file import JsFile
-from analyzer.core.exceptions import InvalidResourceError
-
-import esprima
+import os
 import sys
+import json
+import esprima
+from analyzer.core.exceptions import InvalidResourceError
+from analyzer.datatypes.js_file import JsFile
+from config import ERROR_DUMP_PATH
+
 
 class JsStaticAnalyzer:
 
 	name = "JsStaticAnalyzer"
+
+	def _write_error_file(self, js_file: JsFile, error):
+		save_path = os.path.join(ERROR_DUMP_PATH, js_file.src+".txt")
+		with open(save_path, 'w') as f:
+			f.write(js_file.text)
 
 	def _run_syntactic_extraction(self, js_file: JsFile):
 
@@ -17,7 +25,13 @@ class JsStaticAnalyzer:
 			print("Type of text: ", type(js_file.text))
 			js_file.syntactic_extract = esprima.parse(js_file.text)
 		except Exception as e:
-			raise
+			try:
+				print("Failed first try of syntactic extraction")
+				print("Attempting second try with json.dumps")
+				js_file.syntactic_extract = esprima.parse(json.dumps(js_file.text))
+			except Exception as e2:
+				raise
+
 
 		js_file.synthetic_done = True
 		js_file.static_done = True
@@ -28,6 +42,7 @@ class JsStaticAnalyzer:
 			self._run_syntactic_extraction(js_file)
 		except Exception as e:
 			print("Static Extraction Error: ", e)
+			self._write_error_file(js_file, str(e))
 			js_file.static_run_error = True
 			return False
 		return True
