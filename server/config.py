a48ef04ccd4fcdd5e43f82dd130839711dde815e
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-04-03 16:03:15 +0800

    New variable to specify path for error dump

--- server/config.py
+++ server/config.py
@@ -8,3 +8,6 @@ DYNAMIC_DUMP_FLDR = os.path.join(os.getcwd(), "data", "js_dynamic_results")
 
 ANALYZED_PAGES_SAVE_PATH = os.path.join(os.getcwd(), "save",  "database.pickle")
 PENDING_PAGES_SAVE_PATH = os.path.join(os.getcwd(), "save", "pending.pickle")
+
+
+ERROR_DUMP_PATH = os.path.join(os.getcwd(), "save", "errors")
