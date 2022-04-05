b7f521811eba0f3774fd4cd3cfe43a583919bd45
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-04-05 17:12:35 +0800

    Update analyzer_thread.py

--- server/app/threads/analyzer_thread.py
+++ server/app/threads/analyzer_thread.py
@@ -1,12 +1,13 @@
-
 import itertools
 import time
 from collections import deque
 from typing import List
 
-from app.threads.worker import Worker
 from analyzer.controllers.analysis_controller import AnalysisController
 from analyzer.controllers.features_controller import FeaturesController
+from app.models.processing_status import ProcessingStatus
+from app.threads.worker import Worker
+
 
 class AnalyzerThread(Worker):
 	
@@ -21,22 +22,29 @@ class AnalyzerThread(Worker):
 		while self._do_run:
 			self._thread_lock.acquire()
 			for page in self._pending_pages:
-				if page.status == "crawling" and page.crawl_success and not page.is_analyzed:
+				if page.status == ProcessingStatus.CRAWLING \
+					and page.crawl_success \
+					and not page.is_analyzed:
+
+					page.status = ProcessingStatus.ANALYZING
 					print("Analyzer found one page", page.src)
 					try:
 						page.status = "analyzing"
-						for js_file in itertools.chain(page.internal_js_files, page.external_js_files):
+						for js_file in itertools.chain(page.internal_js_files
+							, page.external_js_files):
 							print("analyzing js file", js_file.src)
 
 							self._analysis_controller.run_static_analysis(js_file)
 							self._analysis_controller.run_dynamic_analysis(js_file)
-							page.status = "extracting features"
+
 							self._features_controller.extract_all_features(js_file)
 							self._analysis_controller.cleanup()
 
+
 						page.is_analyzed = True
 						page.features_extracted = True
-						page.status = "done"
+						
+						# page.status = ProcessingStatus.DONE #Trigger cleanup
 					except Exception as e:
 						print("AnalyzerThread error: ", e)
 						page.status = "error"
