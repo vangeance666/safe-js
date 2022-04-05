67e29804f7113885ec997075f918d6bff58fb627
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-04-05 17:12:44 +0800

    Update cleaner_thread.py

--- server/app/threads/cleaner_thread.py
+++ server/app/threads/cleaner_thread.py
@@ -1,10 +1,10 @@
-
 import itertools
 import threading
 import time
 from collections import deque
 from typing import List
 
+from app.models.processing_status import ProcessingStatus
 from app.threads.worker import Worker
 
 
@@ -18,11 +18,14 @@ class CleanerThread(Worker):
 		while self._do_run:
 			self._thread_lock.acquire()
 			for i, page in enumerate(self._pending_pages):
-				if page.status == "error" or page.status == "done":
+				if page.status == ProcessingStatus.ERROR or page.status == ProcessingStatus.DONE:
 					del self._pending_pages[i]
 					self._analyzed_pages.append(page)
 					break
 			self._thread_lock.release()
 
-			# print("CrawlerThread sleeping for 5 seconds")			
+			print("CrawlerThread sleeping for 5 seconds")			
+
+			print("pending_queue: ", len(self._pending_pages))
+			print("analyzed_pages: ", len(self._analyzed_pages))
 			time.sleep(5)
